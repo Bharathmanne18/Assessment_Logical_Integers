c6f046bc3aef088d2a5f8d9e19a8fdee6f51b6bc
Bharathmanne18 <bharathmanne12@gmail.com>
2024-08-02 17:50:59 +0530

    Update FindEven.java

--- FindEven.java
+++ FindEven.java
@@ -1,5 +1,19 @@
+ 
 public class FindEven{
-// Prepare a Logic to find out that a Digit in a Number should be Even Digit and Print Even Digit
-  // 1298
-  // if we take above number it should print 2 and 8 because both are even digits in a number
-}
+  // Prepare a Logic to find out that a Digit in a Number should be Even Digit and Print Even Digit
+    // 1298
+    // if we take above number it should print 2 and 8 because both are even digits in a number
+    public static void main(String[] args) {
+      int num =1298;
+      while(num > 0)
+      {
+        int rem = num% 10;
+        if(rem % 2 == 0)
+        {
+          System.out.println(rem);
+        }
+        num = num /10;
+      }
+    }
+  }
+  
