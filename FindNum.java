429ff7b764900c37f5c1661cbe7b6db8ae5a6b6f
Bharathmanne18 <bharathmanne12@gmail.com>
2024-08-02 17:44:52 +0530

    Update FindNum.java

--- FindNum.java
+++ FindNum.java
@@ -1,6 +1,22 @@
-public FindNum{
+ 
+import java.util.Scanner;
+public class FindNum{
 // Prepare a Logic to find out 2nd  number in a set of number 
   // Let's Take a Number in Scanner
   // Example if you take 235 as number it should return 3
   // Likewise Logic you have written should get 2nd digit of any number
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number");
+    String a = scan.nextLine();
+    if(a.length() < 2)
+    {
+      System.out.println("The number should contain minimum 2 digits");
+    }
+    else
+    {
+      System.out.println(a.charAt(1));
+    }
+    
+  }
 }
