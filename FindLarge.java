256d0ec65642420ab5cbd511bf823298d0c8127b
Bharathmanne18 <bharathmanne12@gmail.com>
2024-08-02 17:51:30 +0530

    Update FindLarge.java

--- FindLarge.java
+++ FindLarge.java
@@ -1,5 +1,25 @@
+ 
+import java.util.Scanner;
 public class FindLarge{
 // prepare a Logic to findout Large digit in given Number
   // If Number is 1782 The Largest Digit is 8 You should prepare logic to print that Number
   // Take Number By using Scanner
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number");
+    int a = scan.nextInt();
+    int temp = 0;
+    while(a > 0)
+    {
+      int rem = a % 10;
+      if(rem > temp)
+      {
+        temp = rem;
+        
+      }
+      a = a/10;
+      
+    }
+    System.out.println(temp);
+  }
   }
